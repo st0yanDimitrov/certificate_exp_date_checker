3ecde1cfb84a63d4936c70e00cb8e321f8d1b2d1
Stoyan <stdimitrov90@gmail.com>
2024-06-14 17:18:51 +0300

    init + readme

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # sap-certificate-exp-date-checker
-SAP task - Certificate Expiry Date Checker Script
+SAP task - Certificate Expiry Date Checker
+
+The purpose of this script is to retrieve the SSL/TLS certificate's expiration date of a provided host address (could be either URL or IP address)
+The script parameters should be provided as CLI arguments.
+
+## Prerequisites:
+
+OpenSSL (tested with v24.1.0) module needs to be installed prior using the tool.
+
+How to install:
+```sh
+pip install pyOpenSSL==24.1.0
+```
+
+## Syntax:
+
+```txt
+python certificate-exp-date-checker.py --host <target_host> --port <target_port> --timeout <timeout_seconds>
+```
+
+### Arguments
+
+#### Mandatory
+--host
+_Target host. Could be either URL or IP address_
+
+#### Optional
+--port
+_Target port. If not specified, 443 is taken as default_
+
+--timeout (default: 10 seconds)
+_Request timeout. If not specified, 10 seconds is taken as default_
+
+## Examples:
+
+```sh
+python certificate-exp-date-checker.py --host 192.168.0.1
+```
+
+```txt
+SSL certificate's expiration date of 192.168.0.1 is: 2025-03-07 23:59:59
+```
+
+```sh
+python certificate-exp-date-checker.py --host example.com --port 8443 --timeout 5
+```
+
+```txt
+SSL certificate's expiration date of example.com is: 2025-03-07 23:59:59
+```
---
+++ src/certificate_exp_date_checker.py
@@ -0,0 +1,77 @@
+import ssl
+import sys
+from datetime import datetime
+from argparse import ArgumentParser
+from OpenSSL import crypto
+
+
+class InputArgs():
+    host: str
+    port: int
+    timeout: int
+
+
+def get_certificate(host, port, timeout) -> str:
+    
+    # Retrieve the destination SSL certificate public key
+    try:
+        cert_pem: str  = ssl.get_server_certificate((host, port), timeout=timeout)
+    except Exception as e:
+        print (f"A certificate at address {host} couldn't be retreived. {str(e)}")
+        sys.exit(1)
+    else:
+        return cert_pem
+
+
+def get_certicicate_expiration_date(cert) -> str:
+    
+    # Decrypt the input public key
+    try: 
+        cert_x509 = crypto.load_certificate(crypto.FILETYPE_PEM, cert)
+    except Exception as e:
+        print(f"There was an issue with the retreived certificate. {str(e)}")
+        sys.exit(1)
+    else:
+        
+        # Extract the expiration date, format and return as string
+        exp_timestamp: str = cert_x509.get_notAfter().decode("ascii")
+        exp_date: datetime = datetime.strptime(exp_timestamp, "%Y%m%d%H%M%SZ")
+        return(str(exp_date))
+    
+
+def get_arguments() -> InputArgs:
+    
+    # Define a simple CLI tool frame
+    parser: ArgumentParser = ArgumentParser(description="CLI tool to retrieve the SSL certificate's expiration date")
+    parser.add_argument("--host", action="store", dest="host", required=True, help="Destination host")
+    parser.add_argument("--port", action="store", dest="port", default=443, help="Destination port (default 443)")
+    parser.add_argument("--timeout", action="store", dest="timeout", default=10, help="Timeout in seconds (default 10)")
+    
+    # Return the input parameters
+    args: InputArgs = parser.parse_args()
+    return(args)
+
+
+def main():
+    
+    # Get input arguments from CLI
+    args: InputArgs = get_arguments()
+
+    # Kepp the original host input for the result message
+    host_original = args.host
+
+    # Remove protocol and any pages in case provided
+    args.host = args.host.split("://")[1]
+    args.host = args.host.split("/")[0]
+
+    # Retrieve the destination SSL certificate
+    certificate = get_certificate(args.host, args.port, args.timeout)
+    
+    # Extract certificate"s expiration date
+    result =  get_certicicate_expiration_date(certificate)
+
+    # Print the result to CLI
+    print(f"SSL certificate's expiration date of {host_original} is: {result}")
+
+if __name__=="__main__":
+    main()
